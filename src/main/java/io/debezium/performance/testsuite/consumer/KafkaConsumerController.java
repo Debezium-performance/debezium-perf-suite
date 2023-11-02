5481e4b60b15a735f0062d6981830a69e386db52
Jiri Novotny <jnovotny@redhat.com>
2023-11-02 09:56:03 +0100

    WIP Add topic recreation

--- src/main/java/io/debezium/performance/testsuite/consumer/KafkaConsumerController.java
+++ src/main/java/io/debezium/performance/testsuite/consumer/KafkaConsumerController.java
@@ -1,9 +1,18 @@
 package io.debezium.performance.testsuite.consumer;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.Config;
+import org.apache.kafka.clients.admin.ConfigEntry;
+import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.clients.admin.DescribeTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +20,19 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static io.debezium.performance.testsuite.ConfigProperties.KAFKA_BOOTSTRAP_SERVERS;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.CLIENT_ID_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.FETCH_MAX_BYTES_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG;
@@ -31,7 +46,7 @@ import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZE
 public class KafkaConsumerController {
     private final Logger LOG = LoggerFactory.getLogger(KafkaConsumerController.class);
     private static KafkaConsumerController instance;
-    private final Consumer<String, String> consumer;
+    private Consumer<String, String> consumer;
     private KafkaConsumerController() {
         consumer = getNewDefaultConsumer();
     }
@@ -49,12 +64,31 @@ public class KafkaConsumerController {
         while (collection.size() < count) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.of(100, ChronoUnit.SECONDS));
             records.forEach(collection::add);
+            LOG.info("{}",collection.size());
         }
         return collection;
     }
 
+    public void deleteAndRecreateTopic(String topic) {
+        try (AdminClient client = AdminClient.create(getDefaultAdminProperties())) {
+            Config topicConfig = getTopicConfig(client, topic);
+            TopicDescription topicDescription = getTopicDescription(client, topic);
+
+            NewTopic newTopic = new NewTopic(topic, topicDescription.partitions().size(), (short) topicDescription.partitions().get(0).replicas().size());
+            newTopic.configs(transformConfig(topicConfig));
+            LOG.info("Starting topic deletion");
+            client.deleteTopics(Collections.singleton(topic)).all().get(100, TimeUnit.SECONDS);
+            LOG.info("Starting topic creation");
+            client.createTopics(Collections.singleton(newTopic)).all().get(100, TimeUnit.SECONDS);
+            LOG.info("Successfully recreated topic");
+            consumer = getNewDefaultConsumer();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static Consumer<String, String> getNewConsumer(Properties consumerProperties) {
-        return new KafkaConsumer<>(getDefaultConsumerProperties());
+        return new KafkaConsumer<>(consumerProperties);
     }
 
     public static Consumer<String, String> getNewDefaultConsumer() {
@@ -65,7 +99,7 @@ public class KafkaConsumerController {
         Properties consumerProps = new Properties();
         consumerProps.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
         consumerProps.put(GROUP_ID_CONFIG, "2");
-        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
         consumerProps.put(ENABLE_AUTO_COMMIT_CONFIG, true);
         consumerProps.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerProps.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -75,5 +109,32 @@ public class KafkaConsumerController {
         consumerProps.put(FETCH_MIN_BYTES_CONFIG, "1073741824");
         consumerProps.put(RECEIVE_BUFFER_CONFIG, "1073741824");
         return consumerProps;
+    }
+    public static Properties getDefaultAdminProperties() {
+        Properties adminProps = new Properties();
+        adminProps.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
+        adminProps.put(CLIENT_ID_CONFIG, "perf-suite-admin-client");
+        return adminProps;
+    }
+
+    private Map<String, String> transformConfig(Config config) {
+        Map<String, String> map = new HashMap<>();
+        for (ConfigEntry currentConfig : config.entries()) {
+            map.put(currentConfig.name(), currentConfig.value());
+        }
+        return map;
+    }
+
+    private Config getTopicConfig(AdminClient client, String topic) throws ExecutionException, InterruptedException {
+        Collection<ConfigResource> cr = Collections.singleton(new ConfigResource(ConfigResource.Type.TOPIC,topic));
+        DescribeConfigsResult ConfigsResult = client.describeConfigs(cr);
+        return (Config) ConfigsResult.all().get().values().toArray()[0];
+    }
+
+    private TopicDescription getTopicDescription(AdminClient client, String topic) throws ExecutionException, InterruptedException {
+        DescribeTopicsResult result = client.describeTopics(List.of(topic));
+        Map<String, KafkaFuture<TopicDescription>>  values = result.topicNameValues();
+        KafkaFuture<TopicDescription> topicDescription = values.get(topic);
+        return topicDescription.get();
     }
 }
--- src/test/java/io/debezium/performance/testsuite/BasicMongoPrintTest.java
+++ src/test/java/io/debezium/performance/testsuite/BasicMongoPrintTest.java
@@ -5,12 +5,15 @@ import io.debezium.performance.testsuite.deserializer.KafkaRecordParser;
 import io.debezium.performance.testsuite.dmt.BareDmtController;
 import io.debezium.performance.testsuite.model.TimeResults;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,59 +23,70 @@ public class BasicMongoPrintTest {
 
     private final Logger LOG = LoggerFactory.getLogger(BasicMongoPrintTest.class);
 
+    @BeforeClass
+    public static void clearTopic() {
+        KafkaConsumerController.getInstance().deleteAndRecreateTopic(KAFKA_TEST_TOPIC);
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     public void test1() {
-        int count = 100000;
-        int size = 10000;
+        int count = 100;
+        int size = 100000;
         BareDmtController dmt = BareDmtController.getInstance();
-        KafkaConsumerController consumer = KafkaConsumerController.getInstance();
-        LOG.info(dmt.generateMongoBulkLoad(count, 10000, size).toString());
-        List<ConsumerRecord<String, String>> records = consumer.getRecords(KAFKA_TEST_TOPIC, count);
+        KafkaConsumerController kafkaController = KafkaConsumerController.getInstance();
+        LOG.info(dmt.generateMongoBulkLoad(count, 1000000, size).toString());
+        // count +1 because of collection create event <- NOT TRUE CURRENTLY
+        List<ConsumerRecord<String, String>> records = kafkaController.getRecords(KAFKA_TEST_TOPIC, count);
         printResults(records);
     }
 
     @Test
     public void test2() {
-        int count = 10000;
-        int size = 100000;
+        int count = 1000;
+        int size = 10000;
         BareDmtController dmt = BareDmtController.getInstance();
         KafkaConsumerController consumer = KafkaConsumerController.getInstance();
-        LOG.info(dmt.generateMongoBulkLoad(count, 10000, size).toString());
+        LOG.info(dmt.generateMongoBulkLoad(count, 1000000, size).toString());
         List<ConsumerRecord<String, String>> records = consumer.getRecords(KAFKA_TEST_TOPIC, count);
         printResults(records);
     }
 
     @Test
     public void test3() {
-        int count = 1000;
-        int size = 1000000;
+        int count = 10000;
+        int size = 1000;
         BareDmtController dmt = BareDmtController.getInstance();
         KafkaConsumerController consumer = KafkaConsumerController.getInstance();
-        LOG.info(dmt.generateMongoBulkLoad(count, 10000, size).toString());
+        LOG.info(dmt.generateMongoBulkLoad(count, 1000000, size).toString());
         List<ConsumerRecord<String, String>> records = consumer.getRecords(KAFKA_TEST_TOPIC, count);
-        printResults(records);
+        printResultsCsv(records, 3);
     }
 
     @Test
     public void test4() {
-        int count = 100;
-        int size = 10000000;
+        int count = 100000;
+        int size = 100;
         BareDmtController dmt = BareDmtController.getInstance();
         KafkaConsumerController consumer = KafkaConsumerController.getInstance();
-        LOG.info(dmt.generateMongoBulkLoad(count, 10000, size).toString());
+        LOG.info(dmt.generateMongoBulkLoad(count, 1000000, size).toString());
         List<ConsumerRecord<String, String>> records = consumer.getRecords(KAFKA_TEST_TOPIC, count);
-        printResults(records);
+        printResultsCsv(records,4);
     }
 
     @Test
     public void test5() {
-        int count = 100;
-        int size = 10000000;
+        int count = 1000000;
+        int size = 10;
         BareDmtController dmt = BareDmtController.getInstance();
         KafkaConsumerController consumer = KafkaConsumerController.getInstance();
-        LOG.info(dmt.generateMongoBulkLoad(count, 10000, size).toString());
+        LOG.info(dmt.generateMongoBulkLoad(count, 1000000, size).toString());
         List<ConsumerRecord<String, String>> records = consumer.getRecords(KAFKA_TEST_TOPIC, count);
-        printResults(records);
+        printResultsCsv(records, 5);
     }
     //[main] INFO io.debezium.performance.testsuite.BasicMongoPrintTest - 999 Database transaction time=2-10-31 07:57:08.000, Debezium receive time=2-10-31 07:58:00.759, Kafka receive time=2-10-31 07:58:01.257, Debezium read speed=52759, Debezium process speed=498
     private void printResults(List<ConsumerRecord<String, String>> records){
@@ -88,30 +102,52 @@ public class BasicMongoPrintTest {
         }
     }
 
-    private void printResultsCsv(List<ConsumerRecord<String, String>> records){
+    private void printResultsCsv(List<ConsumerRecord<String, String>> records, int testNumber){
         int i = 1;
-        List<String[]> dataline = new ArrayList<>();
-        List<String[]> dataline2 = new ArrayList<>();
+        List<String[]> dataLine = new ArrayList<>();
+        dataLine.add(new String[]{"Debezium read timestamp", "Debezium read speed"});
+        List<String[]> dataLine2 = new ArrayList<>();
+        dataLine2.add(new String[]{"Kafka receive timestamp", "Debezium process speed"});
+        List<String[]> dataLine3 = new ArrayList<>();
+        dataLine3.add(new String[]{"Second", "Number of read messages"});
         for (ConsumerRecord<String, String> record : records) {
             TimeResults results;
+            Long lastSecond = -1L;
+            int count = 0;
+            Long currentSecond;
             try {
                 results = KafkaRecordParser.parseTimeResults(record);
-                dataline.add(new String[] {String.valueOf(results.getDebeziumStartTime()), String.valueOf(results.getDebeziumReadSpeed())});
-                dataline2.add(new String[]{String.valueOf(results.getKafkaReceiveTime()), String.valueOf(results.getDebeziumProcessSpeed())});
+                dataLine.add(new String[] {String.valueOf(results.getDebeziumStartTime()), String.valueOf(results.getDebeziumReadSpeed())});
+                dataLine2.add(new String[]{String.valueOf(results.getKafkaReceiveTime()), String.valueOf(results.getDebeziumProcessSpeed())});
+                currentSecond = Instant.ofEpochMilli(results.getDebeziumStartTime()).truncatedTo(ChronoUnit.SECONDS).toEpochMilli();
+                if (lastSecond.equals(currentSecond)) {
+                    count++;
+                } else {
+                    dataLine3.add(new String[]{String.valueOf(lastSecond), String.valueOf(count)});
+                    lastSecond = currentSecond;
+                    count = 1;
+                }
             } catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
 //            LOG.info(i++ + " " + results);
         }
-        try(PrintWriter pw = new PrintWriter("test_read.csv")) {
-            dataline.stream()
+        try(PrintWriter pw = new PrintWriter(String.format("test_read%s.csv", testNumber))) {
+            dataLine.stream()
                     .map(this::convertToCSV)
                     .forEach(pw::println);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        try(PrintWriter pw = new PrintWriter("test_process.csv")) {
-            dataline2.stream()
+        try(PrintWriter pw = new PrintWriter(String.format("test_process%s.csv", testNumber))) {
+            dataLine2.stream()
+                    .map(this::convertToCSV)
+                    .forEach(pw::println);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try(PrintWriter pw = new PrintWriter(String.format("test_read_per_second%s.csv", testNumber))) {
+            dataLine3.stream()
                     .map(this::convertToCSV)
                     .forEach(pw::println);
         } catch (FileNotFoundException e) {
