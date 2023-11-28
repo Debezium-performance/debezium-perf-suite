3b01cf52715b2f2f105d5ac2df542571c8573968
Ondrej Babec <ond.babec@gmail.com>
2023-11-28 14:47:01 +0100

    Support messages without schema

--- src/main/java/io/debezium/performance/testsuite/ConfigProperties.java
+++ src/main/java/io/debezium/performance/testsuite/ConfigProperties.java
@@ -5,4 +5,5 @@ public final class ConfigProperties {
     public static final String KAFKA_BOOTSTRAP_SERVERS = System.getProperty("test.kafka.bootstrap.servers");
     public static final String KAFKA_TEST_TOPIC = System.getProperty("test.kafka.topic");
     public static final String RESULT_DATABASE = System.getProperty("test.result.database");
+    public static final String SCHEMA_INCLUDED = System.getProperty("test.schema.included");
 }
